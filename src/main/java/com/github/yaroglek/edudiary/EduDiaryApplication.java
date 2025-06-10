00ede922ba64f759cbd860a92d95bb578b380b31
Yaroglek <jkl7589@gmail.com>
2025-06-10 19:12:59 +0500

    Initial commit

---
+++ src/main/java/com/github/yaroglek/edudiary/EduDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.github.yaroglek.edudiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yaroglek/edudiary/EduDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yaroglek.edudiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
